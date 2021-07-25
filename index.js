788fae0b912b18e5cc3bb86989975cd0e0f773c0
Deepak <deepakprasad1113@gmail.com>
2021-07-25 18:41:37 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+// node server to handle socket io connextions
+const io= require('socket.io')(8000,{
+    cors:{
+        origin: '*',
+    }
+});
+
+const users = {};
+
+io.on('connection', socket =>{
+    socket.on('new-user-joined', myname =>{
+        console.log("new user", myname );
+        users[socket.id] = myname;
+        socket.broadcast.emit('user-joined', myname );
+    });
+    socket.on('send', message=>{
+        socket.broadcast.emit('receive',{message: message, myname: users[socket.id]})
+    });
+    socket.on('disconnect', message=>{
+        socket.broadcast.emit('left', users[socket.id]);
+        delete users[socket.id];
+    });
+})
+
